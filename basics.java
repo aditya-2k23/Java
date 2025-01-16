fa196a79759dbd434a956d2d46a77c882662cd17
aditya-2k23 <holaaditya123@gmail.com>
2025-01-16 23:05:12 +0530

    Add user input functionality to basics.java for age input

--- basics.java
+++ basics.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class basics {
     public static void main(String[] args) {
         System.out.println("Welcome to the core java class!");
+
+        // Take user input
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter age");
+
+        int age = myObj.nextInt();
+        System.out.println("Age: " + age);
+
+        myObj.close();
     }
 }
 
