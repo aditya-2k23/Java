d6d6800f2dfd480ab96580c1361bb39974b0a604
aditya-2k23 <holaaditya123@gmail.com>
2025-01-26 18:56:36 +0530

    Add Question4.java to determine customer eligibility for premium membership using a ternary operator

---
+++ assignment/Question4.java
@@ -0,0 +1,22 @@
+/*
+ * Use a ternary operator to determine and display the customer's eligibility status.
+ * Ensure proper handling of variable declarations, data types, and access modifiers.
+ */
+
+package assignment;
+
+import java.util.Scanner;
+
+public class Question4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+        double income = sc.nextDouble();
+        boolean dueStatus = sc.nextBoolean();
+
+        String eligibility = (age >= 18 && income >= 30000 && !dueStatus) ? "You are eligible for premium membership."
+                : "You are not eligible for premium membership.";
+        System.out.println(eligibility);
+    }
+}
