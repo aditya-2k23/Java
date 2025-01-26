11482561e533540960a535b8efcef479b79178c7
aditya-2k23 <holaaditya123@gmail.com>
2025-01-26 19:00:23 +0530

    Remove unnecessary comments from Question5.java to improve code clarity

--- assignment/Question5.java
+++ assignment/Question5.java
@@ -1,25 +1,3 @@
-/*
- * You are managing a system that uses a 4-bit permission scheme to represent the access rights of a user. Each bit represents a specific permission:
-
- * Bit 3 (from left): Read
- * Bit 2: Write
- * Bit 1: Execute
- * Bit 0 (from left): Delete
- * Write a program that:
- *
- * Takes a 4-bit integer (0–15) as the current permissions.
- * Takes a bit position (0 to 3) as input and toggles (reverses)  * the bit at that position using bitwise operators.
- * Displays the new permission set after toggling the bit.
- *
- * Input:
- * Enter current permissions (0-15): 10
- * Enter the bit position to toggle (0-3): 2
- *
- * Output:
- * Original permissions: 1010
- * Updated permissions: 1110
- */
-
 package assignment;
 
 import java.util.Scanner;
