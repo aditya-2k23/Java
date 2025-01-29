293b7abc02f559c2966a9ccd20bda581d2851a46
aditya-2k23 <holaaditya123@gmail.com>
2025-01-29 17:30:48 +0530

    Implement functionality to find and print the longest word from user input in Question6.java

--- assignment/Question6.java
+++ assignment/Question6.java
@@ -1,7 +1,22 @@
 package assignment;
 
+import java.util.Scanner;
+
 public class Question6 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String sentence = sc.nextLine();
 
+        String[] words = sentence.split(" ");
+        String longestWord = "";
+
+        for (String word : words)
+            if (word.length() > longestWord.length())
+                longestWord = word;
+
+        System.out.println(longestWord);
+
+        sc.close();
     }
 }
