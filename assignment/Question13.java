8eda4f4e599f2b36995403c736119a82bd940d79
aditya-2k23 <holaaditya123@gmail.com>
2025-02-10 14:20:22 +0530

    Add Question13.java to implement a program that checks if a string is a palindrome

---
+++ assignment/Question13.java
@@ -0,0 +1,27 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class Question13 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        System.out.println(isPalindrome(str));
+        sc.close();
+    }
+
+    public static boolean isPalindrome(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
